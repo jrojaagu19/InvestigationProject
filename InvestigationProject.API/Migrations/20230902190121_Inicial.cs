7748e5e012bb8cdc6ddd98f5caf1a791ae2c26ed
jzuluamu <juanja2798@gmail.com>
2023-09-02 14:11:07 -0500

    Migración de NuGet y conexión a BD

---
+++ InvestigationProject.API/Migrations/20230902190121_Inicial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestigationProject.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InvestigationProjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameProject = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionProject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InvestigationArea = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvestigationProjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investigators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstitutionalAffiliation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoleProject = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investigators", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvestigationProjects_NameProject",
+                table: "InvestigationProjects",
+                column: "NameProject",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InvestigationProjects");
+
+            migrationBuilder.DropTable(
+                name: "Investigators");
+        }
+    }
+}
